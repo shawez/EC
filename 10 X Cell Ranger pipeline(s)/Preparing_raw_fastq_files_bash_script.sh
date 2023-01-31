9b2aa5685db80609d9af48b3d39a3c3298210d27
Shawez Khan <shawez.jmi@gmail.com>
2023-01-31 23:47:30 +0100

    Update Preparing_raw_fastq_files_bash_script.sh

--- 10 X Cell Ranger pipeline(s)/Preparing_raw_fastq_files_bash_script.sh
+++ 10 X Cell Ranger pipeline(s)/Preparing_raw_fastq_files_bash_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Bash script to convert base call files (BCLs) for each flow cell directory into FASTQ files. 
-## Important Arguments
+# Important Arguments
 # --id: Required. A unique run ID string (e.g., Sample1_Fastq).
 # --run: Required. The path of Illumina BCL run folder.
 # --csv	Optional. Path to a simple CSV with lane, sample, and index columns, which describe the way to demultiplex the flow cell. The index 
@@ -9,6 +9,7 @@
 
 # Note: this requires high resources (ram and processing core),
 # Run cellranger mkfastq for BCL to FASTQ conversion.
+
 cellranger mkfastq --id=Sample1_Fastq \
                      --run=/path/to/BCL \
                      --csv=cellranger-bcl-to-fastq-Sample1.csv
